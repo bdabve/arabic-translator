702a806227394b782f3b21d330e69f95b7f3adf0
bdabve <bdabve@gmail.com>
2023-06-20 14:13:25 +0100

    stylesheet in a separate file

--- translate_qt.py
+++ translate_qt.py
@@ -11,7 +11,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from h_interface import Ui_MainWindow
-from translator import translate
+from translator import Translator
 
 
 class MyApp(QMainWindow):
@@ -21,153 +21,24 @@ class MyApp(QMainWindow):
         # Load the .ui file
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.translator = Translator()
 
         self.setWindowTitle("My Application")
         self.setGeometry(200, 200, 400, 300)
 
         # Connect the button click event
-        self.ui.search_button.clicked.connect(self.search)
+
+        self.ui.input_field.returnPressed.connect(self.translate)
+        self.ui.search_button.clicked.connect(self.translate)
 
         # Set dark style sheet
-        self.setStyleSheet("""
-QMainWindow {
-    background-color: #2C2C2C;
-}
+        with open('./style.qss', 'r') as f:
+            style_sheet = f.read()
+        self.setStyleSheet(style_sheet)
 
-QLabel {
-    color: #FFFFFF;
-    font-family: Monaco;
-    font-size: 12px;
-    padding: 10px;
-    height: 30px;
-}
-
-/* LINE EDIT */
-QLineEdit {
-    background-color: #404040;
-    color: #FFFFFF;
-    border: 1px solid #999999;
-    border-radius: 5px;
-    padding: 8px;
-    height: 30px;
-    font-family: Monaco;
-}
-
-/* PUSH BUTTON */
-QPushButton {
-    background-color: #3C3C3C;
-    color: #FFFFFF;
-    border: none;
-    padding: 8px;
-    border-radius: 5px;
-    height: 30px;
-    font-family: Monaco;
-}
-
-QPushButton:hover {
-    background-color: #4C4C4C;
-}
-
-QPushButton:pressed {
-    background-color: #2C2C2C;
-}
-
-/* LIST WIDGET */
-QListWidget {
-    background-color: #404040;
-    color: #FFFFFF;
-    border: 1px solid #999999;
-    border-radius: 5px;
-    padding: 8px;
-    font-family: Monaco;
-}
-
-QListWidget::item:selected {
-    background-color: #666666;
-}
-
-/* VERTICAL SCROLL BAR */
-QScrollBar:vertical {
-    background-color: #404040;
-    width: 12px;
-    margin: 16px 0 16px 0;
-    border-radius: 6px;
-}
-
-QScrollBar::handle:vertical {
-    background-color: #666666;
-    min-height: 20px;
-    border-radius: 6px;
-}
-
-QScrollBar::handle:vertical:hover {
-    background-color: #888888;
-}
-
-QScrollBar::add-line:vertical {
-    background-color: #404040;
-    height: 16px;
-    subcontrol-position: bottom;
-    subcontrol-origin: margin;
-    border-radius: 6px;
-}
-
-QScrollBar::sub-line:vertical {
-    background-color: #404040;
-    height: 16px;
-    subcontrol-position: top;
-    subcontrol-origin: margin;
-    border-radius: 6px;
-}
-
-QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
-    background-color: #2C2C2C;
-    border-radius: 6px;
-}
-
-/* Horizontal SCROLL BAR */
-QScrollBar:horizontal {
-    background-color: #404040;
-    height: 12px;
-    margin: 0 16px 0 16px;
-    border-radius: 6px;
-}
-
-QScrollBar::handle:horizontal {
-    background-color: #666666;
-    min-width: 20px;
-    border-radius: 6px;
-}
-
-QScrollBar::handle:horizontal:hover {
-    background-color: #888888;
-}
-
-QScrollBar::add-line:horizontal {
-    background-color: #404040;
-    width: 16px;
-    subcontrol-position: right;
-    subcontrol-origin: margin;
-    border-radius: 6px;
-}
-
-QScrollBar::sub-line:horizontal {
-    background-color: #404040;
-    width: 16px;
-    subcontrol-position: left;
-    subcontrol-origin: margin;
-    border-radius: 6px;
-}
-
-QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal {
-    background-color: #2C2C2C;
-    border-radius: 6px;
-}
-        """)
-
-    def search(self):
+    def translate(self):
         search_text = self.ui.input_field.text()
-        translation_result = translate(search_text)
+        translation_result = self.translator.translate(search_text)
         self.ui.list_widget.clear()
         if isinstance(translation_result, dict):
             for key, value in translation_result.items():
--- translator.py
+++ translator.py
@@ -7,16 +7,6 @@ import dotenv
 dotenv.load_dotenv(dotenv.find_dotenv())
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-
-def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,        # this is the degree of randomness of the model's output
-    )
-    return response.choices[0].message["content"]
-
-
 translate_history = [
     {
         'role': 'system',
@@ -36,34 +26,61 @@ Please format your responses to a python dict:
 ]
 
 
-def translate(question):
-    translate_history.append({'role': 'user', 'content': f"{question}"})
-    response = get_completion_from_messages(translate_history)
-    translate_history.append({'role': 'assistant', 'content': f'{response}'})
-    trans_result = parse_translation_response(response)
-    return trans_result
+class Translator:
+    def __init__(self):
+        self.translate_history = [
+            {
+                'role': 'system',
+                'content': """
+As an Arabic translator, your role is to assist with language-related tasks. I will provide you with a variety of words and sentences in different languages. Your tasks include identifying the language, checking for grammar and spelling errors, and translating the content to Arabic.
 
+Please format your responses to a python dict:
+{
+    'source': 'Language identification',
+    'grammar_spelling': 'Grammar and spell check',
+    'Correction': 'Corrected sentence',
+    'Translation': 'Translation of the corrected sentence to Arabic',
+    'Note': 'If you have any one'
+}
+                """
+            }
+        ]
 
-def parse_translation_response(response):
-    # Split the response into individual lines
-    lines = response.strip().split("\n")
-    try:
-        translation_result = {
-            'source': lines[1].split(": ")[1],
-            'grammar_spell_check': lines[2].split(": ")[1],
-            'corrected_sentence': lines[3].split(": ")[1],
-            'translation': lines[4].split(": ")[1]
-        }
+    def get_completion_from_messages(self, messages, model="gpt-3.5-turbo", temperature=0):
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=messages,
+            temperature=temperature,
+        )
+        return response.choices[0].message["content"]
 
-        return translation_result
-    except IndexError:
-        return lines
+    def translate(self, question):
+        self.translate_history.append({'role': 'user', 'content': f"{question}"})
+        response = self.get_completion_from_messages(self.translate_history)
+        self.translate_history.append({'role': 'assistant', 'content': f'{response}'})
+        trans_result = self.parse_translation_response(response)
+        return trans_result
+
+    def parse_translation_response(self, response):
+        lines = response.strip().split("\n")
+        try:
+            translation_result = {
+                'source': lines[1].split(": ")[1],
+                'grammar_spell_check': lines[2].split(": ")[1],
+                'corrected_sentence': lines[3].split(": ")[1],
+                'translation': lines[4].split(": ")[1]
+            }
+
+            return translation_result
+        except IndexError:
+            return lines
 
 
 if __name__ == '__main__':
+    translator = Translator()
     while True:
         question = input('>>> ')
-        resp = translate(question)
+        resp = translator.translate(question)
         print(resp)
         if question in ['exit', 'by']:
             break
